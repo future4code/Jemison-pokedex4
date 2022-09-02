64e8ff94256484a05bcc59b023ec4079dfdae799
sterx17 <disasxster@aol.com>
2022-09-02 15:45:30 -0300

    <build> chamada da api na página home está em progresso. criação da pasta context

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import Router from './components/routes/Router';
+import { baseURL } from './constants/baseURL';
+import { useRequestData} from './hooks/useRequestData'
+import { AllContexts } from './contexts/context'
 
 function App() {
+
+    // chamada da API que será passada como context
+    const [update, setUpdate] = useState(0)
+
+    const [pokemons, isLoading, error] = useRequestData(baseURL, {}, update)
+
   return (
-    <Router />
+    <AllContexts.Provider value={{update, setUpdate, pokemons, isLoading, error}} >
+      <Router />
+    </AllContexts.Provider>
+
   );
 }
 
